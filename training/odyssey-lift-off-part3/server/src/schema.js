ae07fd9cdd10b80bf02d7cff333a099cca2bdc73
R S Chiraag <chiraag.rs@gmail.com>
2025-09-25 15:01:11 +0530

    Feat: complete lift-off p3

--- training/odyssey-lift-off-part3/server/src/schema.js
+++ training/odyssey-lift-off-part3/server/src/schema.js
@@ -4,6 +4,7 @@ const typeDefs = gql`
   type Query {
     "Query to get tracks array for the homepage grid"
     tracksForHome: [Track!]!
+    track(id: ID!): Track
   }
 
   "A track is a group of Modules that teaches about a specific topic"
@@ -19,6 +20,12 @@ const typeDefs = gql`
     length: Int
     "The number of modules this track contains"
     modulesCount: Int
+	  "The track's complete array of modules"
+	  modules: [Module!]!
+    "The count of how many times a track has been viewed"
+    numberOfViews: Int
+    "Contains all the info needed about the track for the user to know"
+    description: String
   }
 
   "Author of a complete Track or a Module"
@@ -29,6 +36,15 @@ const typeDefs = gql`
     "Author's profile picture"
     photo: String
   }
+
+  "A module is a course which is part of a track"
+  type Module {
+    id: ID!
+    "The module's title"
+    title: String!
+    "The module's length in minutes"
+    length: Int
+    }
 `;
 
 module.exports = typeDefs;
