18c986f49c343c240b78638a801074bf07dc931d
R S Chiraag <chiraag.rs@gmail.com>
2025-10-29 16:53:49 +0530

    Feat: Declarations

---
+++ training/typescript/declarations/custom-modules.d.ts
@@ -0,0 +1,8 @@
+declare module "./math.js" {
+	export function add(a: number, b: number): number;
+	export function multiply(a: number, b: number): number;
+}
+
+declare module "./string.js" {
+	export function capitalize(text: string): string;
+}
---
+++ training/typescript/declarations/index.ts
@@ -0,0 +1,7 @@
+import { add, multiply } from "./math.js";
+import { capitalize } from "./string.js";
+
+console.log(`Welcome ${capitalize('Chiraag')}!`);
+
+console.log("Sum:", add(5, 3));
+console.log("Product:", multiply(4, 6));
